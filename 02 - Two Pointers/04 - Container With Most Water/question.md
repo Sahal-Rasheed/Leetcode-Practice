27ef638d9eb6be450bcf5ed84b1e477a14ddcdfd
Sahal Rasheed <sahalrasheed48@gmail.com>
2026-04-08 22:27:56 +0530

    add: implement solution and problem description for "Container With Most Water" challenge

---
+++ 02 - Two Pointers/04 - Container With Most Water/question.md
@@ -0,0 +1,27 @@
+# Container With Most Water
+
+https://neetcode.io/problems/max-water-container/question
+
+![Medium](https://img.shields.io/badge/medium-f5a623?style=for-the-badge&logo=leetcode&logoColor=white)
+
+*You are given an integer array `heights` where `heights[i]` represents the height of the ith bar. The width of each bar is `1`. Find the maximum area of water that can be contained between any two bars.*
+
+
+Example 1:
+```python
+Input: height = [1,7,2,5,4,7,3,6]
+
+Output: 36
+```
+
+Example 2:
+```python
+Input: height = [2,2,2]
+Output: 4
+```
+
+
+Recommended Time & Space Complexity: O(n) & O(1)
+
+Approach: Two Pointers
+___
---
+++ 02 - Two Pointers/04 - Container With Most Water/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxArea(self, heights: list[int]) -> int:
+        l, r = 0, len(heights) - 1  # noqa
+        res = 0
+
+        while l < r:
+            # area is calculated by multiplying the minimum height of the two bars with the distance between them, because the water will be limited by the shorter bar and the width is the distance between the two bars
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1  # noqa
+            else:
+                r -= 1
+        return res
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.maxArea([1, 7, 2, 5, 4, 7, 3, 6]))  # Output: 36
+    print(solution.maxArea([2, 2, 2]))  # Output: 4
+## -- Time & Space Complexity -- ##
+## Time Complexity: O(n)
+## Space Complexity: O(1)
+## ---------------------------- ##
